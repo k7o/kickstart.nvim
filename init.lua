439e95888c993b806ffcb38e066788543029afde
Eric Kerst <eric.kerst@gmail.com>
2024-08-31 14:22:14 +0200

    added yaml lsp config for k8s

--- init.lua
+++ init.lua
@@ -698,30 +698,6 @@ require('lazy').setup({
             },
           },
         },
-        -- yamlls = {
-        --   settings = {
-        --     yaml = {
-        --       schemas = {
-        --         kubernetes = '/*.yaml',
-        --       },
-        --     },
-        --   },
-        -- },
-        -- azure_pipelines_ls = {
-        --   settings = {
-        --     yaml = {
-        --       schemas = {
-        --         ['https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json'] = {
-        --           '/azure-pipeline*.y*l',
-        --           '/*.azure*',
-        --           'Azure-Pipelines/**/*.y*l',
-        --           '.azuredevops/**/*.y*l',
-        --           'Pipelines/*.y*l',
-        --         },
-        --       },
-        --     },
-        --   },
-        -- },
       }
 
       -- Ensure the servers and tools above are installed
@@ -1008,12 +984,8 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,113 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'b0o/schemastore.nvim',
+  },
+  {
+    'tpope/vim-commentary',
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy',
+  },
+  {
+    'someone-stole-my-name/yaml-companion.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      -- https://www.arthurkoziel.com/json-schemas-in-neovim/
+      -- https://github.com/datreeio/CRDs-catalog
+      -- https://github.com/b0o/SchemaStore.nvim/blob/main/lua/schemastore/catalog.lua
+      local cfg = require('yaml-companion').setup {
+        builtin_matchers = {
+          kubernetes = { enabled = true },
+        },
+        schemas = {
+          -- not loaded automatically, manually select with
+          -- :Telescope yaml_schema
+          {
+            name = 'helm.toolkit.fluxcd.io helmrelease_v2',
+            uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/helm.toolkit.fluxcd.io/helmrelease_v2.json',
+          },
+          {
+            name = 'kustomize.toolkit.fluxcd.io kustomization_v2',
+            uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/kustomize.toolkit.fluxcd.io/kustomization_v1.json',
+          },
+          {
+            name = 'source.toolkit.fluxcd.io gitrepository_v1',
+            uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/source.toolkit.fluxcd.io/gitrepository_v1.json',
+          },
+          {
+            name = 'source.toolkit.fluxcd.io helmchart_v1',
+            uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/source.toolkit.fluxcd.io/helmchart_v1.json',
+          },
+          {
+            name = 'source.toolkit.fluxcd.io helmrepository_v1',
+            uri = 'hhttps://raw.githubusercontent.com/datreeio/CRDs-catalog/main/source.toolkit.fluxcd.io/helmrepository_v1.json',
+          },
+          {
+            name = 'source.toolkit.fluxcd.io ocirepository_v1beta2',
+            uri = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/source.toolkit.fluxcd.io/ocirepository_v1beta2.json',
+          },
+          -- schemas below are automatically loaded, but added
+          -- them here so that they show up in the statusline
+          {
+            name = 'Kustomization',
+            uri = 'https://json.schemastore.org/kustomization.json',
+          },
+          {
+            name = 'GitHub Workflow',
+            uri = 'https://json.schemastore.org/github-workflow.json',
+          },
+          {
+            name = 'Azure Pipelines',
+            uri = 'https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json',
+          },
+        },
+        lspconfig = {
+          settings = {
+            yaml = {
+              validate = true,
+              schemaStore = {
+                enable = false,
+                url = '',
+              },
+
+              -- schemas from store, matched by filename
+              -- loaded automatically
+              schemas = require('schemastore').yaml.schemas {
+                select = {
+                  'kustomization.yaml',
+                  'GitHub Workflow',
+                  'Azure Pipelines',
+                },
+              },
+            },
+          },
+        },
+      }
+
+      require('lspconfig')['yamlls'].setup(cfg)
+      require('telescope').load_extension 'yaml_schema'
+      -- get schema for current buffer
+      local function get_schema()
+        local schema = require('yaml-companion').get_buf_schema(0)
+        if schema.result[1].name == 'none' then
+          return ''
+        end
+        return schema.result[1].name
+      end
+      require('lualine').setup {
+        sections = {
+          lualine_x = { 'fileformat', 'filetype', get_schema },
+        },
+      }
+    end,
+  },
+}
